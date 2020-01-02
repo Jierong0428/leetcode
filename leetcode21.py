d3b5c33f417838ef7b7e7679581874e55a19867f
Jierong0428 <54598244+Jierong0428@users.noreply.github.com>
2020-01-01 21:18:37 -0500

    leetcode 21

---
+++ leetcode21.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  1 21:18:04 2020
+
+@author: luojierong
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        res = ListNode(0)
+        p = res
+        while l1 and l2:
+            if l1.val < l2.val:
+                p.next = l1
+                l1 = l1. next
+            else:
+                p.next = l2
+                l2 = l2.next
+            p = p.next
+        if l1:
+            p.next = l1
+        if l2:
+            p.next = l2
+        return res.next
